b6060a584ca3226c2cf843486d197a2f65767357
josetedd <josetedd@gmail.com>
2018-11-14 19:38:57 +0300

    Added views to add employee activity

--- app/src/main/java/com/example/root/employeetracker/AddEmployee.java
+++ app/src/main/java/com/example/root/employeetracker/AddEmployee.java
@@ -2,6 +2,9 @@ package com.example.root.employeetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddEmployee extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class AddEmployee extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee);
+
+        // Initialize views
+        final EditText empName = findViewById(R.id.empName);                      // edittext with employee name
+        final EditText empLocation = findViewById(R.id.empLocation);              // edittext with employee Location
+        final EditText empDesignation = findViewById(R.id.empDesignation);        // edittext with employee Designation
+        Button saveBtn = findViewById(R.id.saveBtn);                        // save button
+
+        // create a button listener for the save button
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check user input
+                if(empName.getText().length()==0){
+                    empName.setError("Empty Name");
+
+                }
+                else if (empLocation.getText().length()==0){
+                    empLocation.setError("Empty Location");
+                }
+                else if (empDesignation.getText().length()==0){
+                    empDesignation.setError("Empty Designation");
+
+                }
+                else {
+
+                    // save the data on sqlite by calling the sqlitehelper
+
+                }
+            }
+        });
     }
 }
